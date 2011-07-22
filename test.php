251318999280c1987542bae5b927a2c143965603
faran <faran@WIN-QD2UB943E18>
2011-07-22 16:32:48 -0400

    test commit

--- test.php
+++ test.php
@@ -2,21 +2,4 @@
 include "server/data/DAO/UserDAO.php";
 include "server/data/DAO/RoomDAO.php";
 
-/*$userDAO = new UserDAO();
-
-$userVO = new UserVO();
-
-$userVO->setUserName("faran");
-$userVO->setEmail("faraneg1@umbc.edu");
-$userDAO->insert($userVO); */
-
-$roomDAO = new RoomDAO();
-
-$vo = new RoomVO();
-$vo->setRoomURL("www.google.com");
-$vo->setRoomTitle("Google!");
-//$roomDAO->insert($vo);
-echo "inserted!<br/>";
-
-//echo $roomDAO->roomExists("www.google.com");
-print_r($roomDAO->getAll());
+//This is my test
